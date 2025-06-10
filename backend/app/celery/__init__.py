dac66f3aab52ff86fa6c33985116e9368e594a9e
Feyi <feyibadejo31@gmail.com>
2025-06-11 00:24:36 +0100

    Setup

--- backend/app/celery/__init__.py
+++ backend/app/celery/__init__.py
@@ -1,19 +1,25 @@
+import os
 from celery import Celery
 
 
 def create_celery(app_name=__name__):
-    return Celery(app_name, broker='redis://localhost:6379/0')
+    # Use new-style config keys pulled from environment variables
+    return Celery(
+        app_name,
+        broker=os.environ.get("broker_url", "redis://redis:6379/0"),
+        backend=os.environ.get("CELERY_BACKEND"),
+        broker_connection_retry_on_startup=os.environ.get("broker_connection_retry_on_startup", "True") == "True"
+    )
 
 
 def init_celery(celery, app):
     celery.conf.update(app.config)
-    TaskBase = celery.Task
 
-    class ContextTask(TaskBase):
+    class ContextTask(celery.Task):
         abstract = True
 
         def __call__(self, *args, **kwargs):
             with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
+                return super().__call__(*args, **kwargs)
 
     celery.Task = ContextTask
